6516c6977a64d2f34285a08fb1f5bc29100be0b7
idejie <i@idejie.com>
2024-12-20 17:35:33 +0800

    Update lr_warmup.py

--- mml/utils/lr_warmup.py
+++ mml/utils/lr_warmup.py
@@ -11,7 +11,7 @@ class LRWarmup:
     def __init__(self, epochs, max_lr, k):
         assert k < 0.95 and k > 0.05, "k must be between 0.05 and 0.95"
         self.epochs = epochs
-        self.max_lr = max_lr
+        self.max_lr = 1 # use ratio for the new lambda scheduler
         self.max_point = int(k * self.epochs)
 
     def __call__(self, epoch):
